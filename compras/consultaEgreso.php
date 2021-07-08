a24253087b95ead09c3cae5e87b38f60fbb6e65e
miltongiovanni <miltongiovanni@gmail.com>
2021-07-08 07:06:12 -0400

    Fix consultas

--- compras/consultaEgreso.php
+++ compras/consultaEgreso.php
@@ -11,13 +11,25 @@ spl_autoload_register('cargarClases');
 
 foreach ($_POST as $nombre_campo => $valor) {
     ${$nombre_campo} = $valor;
-    if(is_array($valor)){
+    if (is_array($valor)) {
         //echo $nombre_campo.print_r($valor).'<br>';
-    }else{
+    } else {
         //echo $nombre_campo. '=' .${$nombre_campo}.'<br>';
     }
 }
-$egresoOperador= new EgresoOperaciones();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Seleccionar Comprobante de egreso a Modificar</title>
+    <link href="../css/formatoTabla.css" rel="stylesheet" type="text/css">
+    <script src="../node_modules/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="../js/validar.js"></script>
+</head>
+<body>
+<?php
+$egresoOperador = new EgresoOperaciones();
 if (!$egresoOperador->isValidIdEgreso($idEgreso)) {
     $ruta = "buscarEgreso.php";
     $mensaje = "El número del egreso no es válido, vuelva a intentar de nuevo";
@@ -31,6 +43,6 @@ if (!$egresoOperador->isValidIdEgreso($idEgreso)) {
     mover_pag($ruta, $mensaje, $icon);
 }
 
-
-
 ?>
+</body>
+</html>
