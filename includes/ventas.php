dab7cb371d6ec4c93b1dbc3512e1724abc462dc8
miltongiovanni <miltongiovanni@gmail.com>
2021-11-25 19:12:43 -0500

    Fix retencion en la fuente factura

--- includes/ventas.php
+++ includes/ventas.php
@@ -67,7 +67,7 @@ function calcularTotalesFactura($idFactura, $tasaDescuento)
     } else {
         $reteiva = 0;
     }
-    if (($subtotal >= BASE_C) /*|| ($retFte == 1)*/) {
+    if (($subtotal >= BASE_C) && ($retFte == 1)) {
         $retefuente = round(($subtotal - $descuento) * 0.025,2);
         if (($ciudadCliente == 1) && ($idCatCliente != 1)) {
             $reteica = round(($subtotal - $descuento) * 0.01104,2);
