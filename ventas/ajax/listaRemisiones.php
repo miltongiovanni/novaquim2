f518a0891452ab2f0e6693fb71bc539954fabbb0
miltongiovanni <miltongiovanni@gmail.com>
2022-07-04 07:31:29 -0400

    datatables server processing remisiones

--- ventas/ajax/listaRemisiones.php
+++ ventas/ajax/listaRemisiones.php
@@ -6,8 +6,23 @@ function cargarClases($classname)
 }
 
 spl_autoload_register('cargarClases');
+
+$columns = array(
+    array('db' => 'idRemision', 'dt' => 'idRemision'),
+    array('db' => 'fechaRemision', 'dt' => 'fechaRemision'),
+    array('db' => 'nomCliente', 'dt' => 'nomCliente'),
+    array('db' => 'nomSucursal', 'dt' => 'nomSucursal'),
+    array('db' => 'idPedido', 'dt' => 'idPedido'),
+    array('db' => 'fechaPedido', 'dt' => 'fechaPedido'),
+);
+
+$bindings = array();
+$limit = SSP::limit($_GET, $columns);
+$order = SSP::order($_GET, $columns);
+$where = SSP::filter($_GET, $columns, $bindings);
 $remisionOperador = new RemisionesOperaciones();
-$remisiones = $remisionOperador->getTableSalidaRemisiones();
+$totalRemisiones = $remisionOperador->getTotalTableSalidaRemisiones($where, $bindings);
+$remisiones = $remisionOperador->getTableSalidaRemisiones($limit, $order, $where, $bindings);
 $detRemisionOperador = new DetRemisionesOperaciones();
 for($i=0;$i<count($remisiones); $i++){
     $remisiones[$i]['detRemision']= $detRemisionOperador->getTableDetRemisionFactura($remisiones[$i]['idRemision']);
@@ -15,8 +30,8 @@ for($i=0;$i<count($remisiones); $i++){
 
 $datosRetorno = array(
     'draw' => 0,
-    'recordsTotal' => count($remisiones),
-    'recordsFiltered' => count($remisiones),
+    'recordsTotal' => intval($totalRemisiones),
+    'recordsFiltered' => intval($totalRemisiones),
     'data' => $remisiones
 );
 print json_encode($datosRetorno);
